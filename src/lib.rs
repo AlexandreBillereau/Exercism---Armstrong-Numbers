7413eb801a2240f53992ca24a0cb4718dfe13f44
alxeandre <alexandre.mr.billerau@gmail.com>
2023-03-25 14:10:18 -0400

    finish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {num} is an armstrong number")
+    let numbers: Vec<u64> = num
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect();
+
+    let mut is_armstrong: u64 = 0;
+    numbers
+        .iter()
+        .for_each(|x| is_armstrong += x.pow(numbers.len() as u32));
+
+    if is_armstrong == num as u64 {
+        return true;
+    }
+    false
 }
